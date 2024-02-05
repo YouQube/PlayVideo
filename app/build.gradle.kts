7531682ef53954cd12e2729be07a98e6cb8cc925
LB-Brandon <rmsdud623@gmail.com>
2024-02-06 00:13:57 +0900

    [Build] 뷰 바인딩 + 라이브러리 의존성 추가
    - Retrofit2
    - Glide
    - Fragment-ktx
    - Timber(logging)
    - Coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,6 @@ android {
     defaultConfig {
         applicationId = "com.brandon.playvideo_app"
         minSdk = 33
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,6 +29,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,4 +43,24 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // by viewModels()
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    // timber for logging
+    implementation ("com.jakewharton.timber:timber:5.0.1")
+
+    // 코루틴
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
 }
