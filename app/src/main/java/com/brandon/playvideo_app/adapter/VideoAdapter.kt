4975cc3b6cb2916a0b158b0a980547b57f7ca6cf
LEE HO <dlho1222@naver.com>
2024-02-06 15:15:45 +0900

    #5[Feat] api 리사이클러뷰 연결

--- app/src/main/java/com/brandon/playvideo_app/adapter/VideoAdapter.kt
+++ app/src/main/java/com/brandon/playvideo_app/adapter/VideoAdapter.kt
@@ -1,25 +1,37 @@
 package com.brandon.playvideo_app.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.brandon.playvideo_app.data.Item
 import com.brandon.playvideo_app.databinding.ItemTrendBinding
+import com.bumptech.glide.Glide
 
-class VideoAdapter:RecyclerView.Adapter<VideoAdapter.VideoHolder>() {
-    inner class VideoHolder(private val binding:ItemTrendBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(item:ItemTrendBinding){
+class VideoAdapter(private val items: List<Item>) :
+    RecyclerView.Adapter<VideoAdapter.VideoHolder>() {
+    inner class VideoHolder(private val binding: ItemTrendBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Item) {
+            with(binding) {
+                tvVideoTitle.text = item.snippet.title
+                tvChannelTitle.text = item.snippet.channelTitle
+                Glide.with(itemView).load(item.snippet.thumbnails.maxres.url).into(ivThumbnail)
+            }
 
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoHolder {
-        TODO("Not yet implemented")
+        val inflater =
+            parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val binding = ItemTrendBinding.inflate(inflater, parent, false)
+        return VideoHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: VideoHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(items[position])
     }
 }
